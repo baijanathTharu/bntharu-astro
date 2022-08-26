a48f833c55c62d1613ddf516a35bf7600d567317
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-08-26 14:20:30 +0545

    :sparkles: update configs

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Baijanath Tharu.';
+export const SITE_DESCRIPTION = 'I am a Software developer from Nepal.';
