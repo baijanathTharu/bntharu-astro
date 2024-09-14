ad1cb6f00d8cd56e631005739d9b6c356d518bfb
baijanathTharu <bnchaudhary.tharu@gmail.com>
2024-09-14 22:38:21 +0545

    google analytics integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
-import { remarkReadingTime } from './remark-reading-time.mjs';
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bntharu.com.np/',
+  site: "https://bntharu.com.np/",
   integrations: [
     mdx(),
     sitemap(),
     tailwind({
       config: {
         applyBaseStyles: false,
+      },
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
       },
     }),
   ],
