02395c1dca62c680d1a2aa9c7a4a6783b347fddc
baijanathTharu <bnchaudhary.tharu@gmail.com>
2024-10-02 22:17:46 +0545

    update layout and themes

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Baijanath Tharu.';
-export const SITE_DESCRIPTION = 'I am a Software developer from Nepal.';
+export const SITE_TITLE = "Baijan.";
+export const SITE_DESCRIPTION = "I am a Software developer from Nepal.";
 export const SOCIAL_LINKS = {
-  github: 'https://www.github.com/baijanathTharu',
-  twitter: 'https://www.twitter.com/baijanaththaru',
-  linkedin: 'https://www.linkedin.com/in/bntharu',
+  github: "https://www.github.com/baijanathTharu",
+  twitter: "https://www.twitter.com/baijanaththaru",
+  linkedin: "https://www.linkedin.com/in/bntharu",
 };
